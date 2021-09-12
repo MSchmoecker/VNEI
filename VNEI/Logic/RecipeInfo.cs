81561b9e4956e4e75277b366b0f1d88ddb489337
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2021-09-12 10:00:07 +0200

    fixed plants where not filtered correcty

--- VNEI/Logic/RecipeInfo.cs
+++ VNEI/Logic/RecipeInfo.cs
@@ -12,14 +12,43 @@ namespace VNEI.Logic {
         public bool isOnBlacklist;
 
         public class Part {
-            public Item item;
-            public Amount amount;
-            public int quality;
+            public readonly Item item;
+            public readonly Amount amount;
+            public readonly int quality;
 
             public Part(Item item, Amount amount, int quality) {
                 this.item = item;
                 this.amount = amount;
                 this.quality = quality;
+            }
+
+            protected bool Equals(Part other) {
+                return Equals(item, other.item) && amount.Equals(other.amount) && quality == other.quality;
+            }
+
+            public override bool Equals(object obj) {
+                if (ReferenceEquals(null, obj)) {
+                    return false;
+                }
+
+                if (ReferenceEquals(this, obj)) {
+                    return true;
+                }
+
+                if (obj.GetType() != GetType()) {
+                    return false;
+                }
+
+                return Equals((Part)obj);
+            }
+
+            public override int GetHashCode() {
+                unchecked {
+                    int hashCode = (item != null ? item.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ amount.GetHashCode();
+                    hashCode = (hashCode * 397) ^ quality;
+                    return hashCode;
+                }
             }
         }
 
