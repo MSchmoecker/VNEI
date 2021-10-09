4691840d92c89ea0f3d322bad11577fa375e3c73
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2021-10-09 16:54:44 +0200

    Readme informs about publicized assemblies

--- deploy.sh
+++ deploy.sh
@@ -22,11 +22,12 @@ done < Environment.props
 
 # set ModDir
 if $USE_R2MODMAN_AS_DEPLOY_FOLDER; then
-	PluginFolder="$R2MODMAN_INSTALL/BepInEx/plugins"
+  BEPINEX_INSTALL="$R2MODMAN_INSTALL/BepInEx"
 else
-	PluginFolder="$VALHEIM_INSTALL/BepInEx/plugins"
+  BEPINEX_INSTALL="$VALHEIM_INSTALL/BepInEx"
 fi
 
+PluginFolder="$BEPINEX_INSTALL/plugins"
 ModDir="$PluginFolder/$ModName"
 
 # copy to unity
@@ -35,11 +36,11 @@ mkdir -p "$ModNameUnity/AssetBundles/StandaloneWindows"
 
 cp "$ModName/obj/Debug/$ModName.dll" "$ModNameUnity/Assets/Assemblies"
 
-cp "$VALHEIM_INSTALL/BepInEx/core/BepInEx.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/core/0Harmony.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/core/Mono.Cecil.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/core/MonoMod.Utils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/core/MonoMod.RuntimeDetour.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/core/BepInEx.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/core/0Harmony.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/core/Mono.Cecil.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/core/MonoMod.Utils.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/core/MonoMod.RuntimeDetour.dll" "$ModNameUnity/Assets/Assemblies"
 [ -f "$PluginFolder/ValheimModding-Jotunn/Jotunn.dll" ] && cp "$PluginFolder/ValheimModding-Jotunn/Jotunn.dll" "$ModNameUnity/Assets/Assemblies"
 [ -f "$PluginFolder/Jotunn/Jotunn.dll" ] && cp "$PluginFolder/Jotunn/Jotunn.dll" "$ModNameUnity/Assets/Assemblies"
 cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_valheim.dll" "$ModNameUnity/Assets/Assemblies"
@@ -49,13 +50,13 @@ cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_sunshafts.dll" "$ModNameUnity
 cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_guiutils.dll" "$ModNameUnity/Assets/Assemblies"
 cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_steamworks.dll" "$ModNameUnity/Assets/Assemblies"
 cp "$VALHEIM_INSTALL/valheim_Data/Managed/assembly_googleanalytics.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_valheim.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_utils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_postprocessing.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_sunshafts.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_guiutils.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_steamworks.dll" "$ModNameUnity/Assets/Assemblies"
-cp "$VALHEIM_INSTALL/BepInEx/plugins/MMHOOK/MMHOOK_assembly_googleanalytics.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_valheim.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_utils.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_postprocessing.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_sunshafts.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_guiutils.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_steamworks.dll" "$ModNameUnity/Assets/Assemblies"
+cp "$BEPINEX_INSTALL/plugins/MMHOOK/MMHOOK_assembly_googleanalytics.dll" "$ModNameUnity/Assets/Assemblies"
 echo Coping to: "$ModDir"
 
 # copy content
