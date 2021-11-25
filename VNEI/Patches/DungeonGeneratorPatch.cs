1ad31f235a3b906ea7990fea9ca5672a62ca7825
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2021-11-25 19:05:40 +0100

    moved Indexing call even further back to let mods more time

--- VNEI/Patches/DungeonGeneratorPatch.cs
+++ VNEI/Patches/DungeonGeneratorPatch.cs
@@ -3,8 +3,8 @@ using VNEI.Logic;
 
 namespace VNEI.Patches {
     [HarmonyPatch]
-    public static class DungeonDBPatch {
-        [HarmonyPatch(typeof(DungeonDB), nameof(DungeonDB.Start)), HarmonyPostfix]
+    public static class DungeonGeneratorPatch {
+        [HarmonyPatch(typeof(DungeonGenerator), nameof(DungeonGenerator.Load)), HarmonyPostfix]
         public static void Start() {
             Indexing.IndexAll();
         }
