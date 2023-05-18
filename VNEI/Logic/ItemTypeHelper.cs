0cd684103fb3dd49e2ddba697aeee7afcd68d217
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-05-18 16:12:26 +0200

    fix: categories torches as weapons and exclude TorchMist

--- VNEI/Logic/ItemTypeHelper.cs
+++ VNEI/Logic/ItemTypeHelper.cs
@@ -4,16 +4,18 @@ namespace VNEI.Logic {
             switch (itemData.m_shared.m_itemType) {
                 case ItemDrop.ItemData.ItemType.Consumable:
                     return ItemType.Food;
-                case ItemDrop.ItemData.ItemType.Chest:
-                case ItemDrop.ItemData.ItemType.Hands:
                 case ItemDrop.ItemData.ItemType.Helmet:
+                case ItemDrop.ItemData.ItemType.Chest:
                 case ItemDrop.ItemData.ItemType.Legs:
+                case ItemDrop.ItemData.ItemType.Hands:
                 case ItemDrop.ItemData.ItemType.Shoulder:
                     return ItemType.Armor;
                 case ItemDrop.ItemData.ItemType.OneHandedWeapon:
-                case ItemDrop.ItemData.ItemType.TwoHandedWeapon:
                 case ItemDrop.ItemData.ItemType.Bow:
                 case ItemDrop.ItemData.ItemType.Shield:
+                case ItemDrop.ItemData.ItemType.TwoHandedWeapon:
+                case ItemDrop.ItemData.ItemType.TwoHandedWeaponLeft:
+                case ItemDrop.ItemData.ItemType.Torch:
                     return ItemType.Weapon;
                 default:
                     return ItemType.Item;
